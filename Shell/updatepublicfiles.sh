65637f35f020feaab84436ee77c8d752fb3df1e7
Mike <dm5xx@gmx.de>
2021-02-20 11:51:20 +0100

    w

--- Shell/updatepublicfiles.sh
+++ Shell/updatepublicfiles.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo git -C /home/shares/ubs/public/ fetch --all
+sudo git -C /home/shares/ubs/public/ fetch --all https://github.com/dm5xx/minimalRaspi_Public.git
 sudo git -C /home/shares/ubs/public/ reset --hard origin/master
+sudo git -C /home/shares/ubs/public/ pull https://github.com/dm5xx/minimalRaspi_Public.git
 echo "Public Update done - please start Server";
