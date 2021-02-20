6abfb1991c39e81a22738ab802bb6e8c3c2d2f4c
Mike <dm5xx@gmx.de>
2021-02-20 11:47:43 +0100

    changed updatepublicfiles to hard override

--- Shell/updatepublicfiles.sh
+++ Shell/updatepublicfiles.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo git -C /home/shares/ubs/public/ pull https://github.com/dm5xx/minimalRaspi_Public.git
+sudo git -C /home/shares/ubs/public/ fetch --all
+sudo git -C /home/shares/ubs/public/ reset --hard origin/master
 echo "Public Update done - please start Server";
